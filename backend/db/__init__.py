600a3a90eb08fede46ca39daea2df0006cadbee6
Aniket-Dhavale-official <aniketvdhavale1801@gmail.com>
2026-01-28 18:48:51 +0530

    Folder restructured

--- backend/db/__init__.py
+++
@@ -1,3 +0,0 @@
-from .mongo import init_db, get_db_client
-
-__all__ = ["init_db", "get_db_client"]
--- backend/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import UserModel  # noqa: F401
-from .video import VideoModel  # noqa: F401
-
-__all__ = ["UserModel", "VideoModel"]
-
